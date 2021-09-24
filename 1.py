72e679864b2a4f59ccdff59790ee514c6f104363
Devnandan  S <devndn8900@gmail.com>
2021-09-24 16:03:34 +0530

    Question 1

---
+++ 1.py
@@ -0,0 +1,21 @@
+# Q. https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements/0/?problemType=full&page=1&sortBy=submissions&query=problemTypefullpage1sortBysubmissions
+
+t = int(input())
+
+for i in range(t):
+    n,d = map(int,input().split())
+    
+    ar = list(map(int,input().split()))
+    
+    
+    for i in range(d,n):
+        
+        
+        print(ar[i],end=" ")
+        
+    for i in range(d):
+        
+        print(ar[i], end = " ")
+    
+    
+    print(" ")
