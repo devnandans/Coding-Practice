69203f859c64ba31779293e2455f4812a8a79b92
Devnandan  S <devndn8900@gmail.com>
2021-09-30 23:10:55 +0530

    Question 22

---
+++ 22.py
@@ -0,0 +1,19 @@
+#You are given a cubic dice with 6 faces. All the individual faces have a number printed on them. The numbers are in the range of 1 to 6, like any ordinary dice. You will be provided with a face of this cube, your task is to guess the number on the opposite face of the cube.
+
+class Solution:
+    def oppositeFaceOfDice(self, N):
+    	#code here 
+    	return (6-N)+1;
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.oppositeFaceOfDice(N)
+        print(ans)
+# } Driver Code Ends
